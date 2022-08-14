4931b0d6fd6fcc46eb099024f088a3ba105e52db
xyh911 <1259103667@qq.com>
2022-08-14 21:46:46 +0800

    feat:学习threejs

---
+++ src/assets/ts/TEngine.ts
@@ -0,0 +1,67 @@
+import {AmbientLight, AxesHelper, BoxBufferGeometry, GridHelper, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, Vector3, WebGLRenderer} from 'three';
+import Stats from 'three/examples/jsm/libs/stats.module';
+export class TEngine{
+    private dom:HTMLElement
+    private renderer : WebGLRenderer
+
+    private scene : Scene
+    private camera : PerspectiveCamera
+    constructor(dom:HTMLElement){
+        // 初始化
+        this.dom = dom
+        this.renderer = new WebGLRenderer()
+        this.scene = new Scene()
+        this.camera = new PerspectiveCamera(45,dom.offsetWidth/dom.offsetHeight,1,1000)
+        // 相机初始化
+        this.camera.position.set(20,20,20)
+        this.camera.lookAt(new Vector3(0,0,0))
+        this.camera.up = new Vector3(0,1,0)
+
+        // 大小设定
+        this.renderer.setSize(dom.offsetWidth,dom.offsetHeight,true)
+        // 创建box
+        const box = new Mesh(
+            new BoxBufferGeometry(10,10,10),
+            new MeshStandardMaterial({
+                color: 'rgb(255,0,0)'
+            })
+        )
+        // 光
+        const ambientLight = new AmbientLight('rgb(255,255,255)',1)
+        // 辅助
+        const axesHepler = new AxesHelper(500)
+        const gridHelper = new GridHelper(500,20,'rgb(200,200,200)','rgb(100,100,100')
+
+        this.scene.add(box)
+        this.scene.add(ambientLight)
+        this.scene.add(axesHepler)
+        this.scene.add(gridHelper)
+
+        // this.renderer.setClearColor('rgb(255,255,255)')
+        // this.renderer.clearColor()
+        // this.renderer.render(this.scene,this.camera)
+
+        // 初始性能监视器
+        const stats = Stats()
+        const statsDom = stats.domElement
+        statsDom.style.position = 'fixed'
+        statsDom.style.top = '0xp'
+        statsDom.style.right = '5px'
+        statsDom.style.left = 'unset'
+
+        const renderFun = ()=>{
+            box.position.x += -0.01
+            box.rotation.y += 0.001
+            this.camera.position.x += -0.01
+            this.renderer.render(this.scene,this.camera)
+            stats.update()
+            requestAnimationFrame(renderFun)
+            
+        }
+        renderFun()
+
+
+        dom.appendChild(this.renderer.domElement)
+        dom.appendChild(statsDom)
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import  './assets/common.css';
+createApp(App).mount('#app')
